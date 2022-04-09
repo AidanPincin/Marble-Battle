12f44f71165b6f2967b1680e964ea43d9a5359f8
AidanPincin <93996840+AidanPincin@users.noreply.github.com>
2022-04-09 11:52:43 -0400

    Towers can capture each other
    If one of the corner squares changes color the tower next to that square will change color accordingly. also i couldn't get the tower firing to be 100% accurate so some towers shoot sorta sideways.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 function drawRect(color,x,y,w,h){ctx.fillStyle = color;ctx.fillRect(x,y,w,h)}
 const sqaureSize = 19
+const marbleSpeed = 15
+let Chance = 0.75
 class Square{
     constructor(x,y,color){
         this.x = x
@@ -12,7 +14,7 @@ class Square{
     }
     draw(){drawRect(this.color,this.x,this.y,this.width,this.height)}
     collide(x,y,width,height,color){
-        if (x<=this.x+this.width && x+width>=this.x && y<=this.y+this.height && y+height>=this.y){
+        if (x-width/2<=this.x+this.width && x+width*1.5>=this.x && y-height/2<=this.y+this.height && y+height*1.5>=this.y){
             if (this.color != color){this.color = color; this.draw(); return true}
             else{this.draw(); return false}
         }
@@ -34,30 +36,35 @@ class Marble{
         let a = angle*20*Math.PI
         if (a<0){a+=360}
         if (a<90 && a>0){
-            this.y_speed = a/90*20
-            this.x_speed = 20-this.y_speed
+            this.y_speed = a/90*marbleSpeed
+            this.x_speed = (marbleSpeed-this.y_speed)/Math.sqrt(Math.sqrt(size))
+            this.y_speed /= Math.sqrt(Math.sqrt(size))
         }
         if (a<360 && a>270){
-            this.x_speed = (a-270)/90*20
-            this.y_speed = this.x_speed-20
+            this.x_speed = (a-270)/90*marbleSpeed
+            this.y_speed = (this.x_speed-marbleSpeed)/Math.sqrt(Math.sqrt(size))
+            this.x_speed /= Math.sqrt(Math.sqrt(size))
         }
         if (a<180 && a>90){
-            this.x_speed = -(a-90)/90*20
-            this.y_speed = 20+this.x_speed
+            this.x_speed = -(a-90)/90*marbleSpeed
+            this.y_speed = (marbleSpeed+this.x_speed)/Math.sqrt(Math.sqrt(size))
+            this.x_speed /= Math.sqrt(Math.sqrt(size))
         }
         if (a>180 && a<270){
-            this.y_speed = -(a-180)/90*20
-            this.x_speed = -(20-(this.y_speed*-1))
-            console.log(this.x_speed,this.y_speed)
+            this.y_speed = -(a-180)/90*marbleSpeed
+            this.x_speed = (-(marbleSpeed-(this.y_speed*-1)))/Math.sqrt(Math.sqrt(size))
+            this.y_speed /= Math.sqrt(Math.sqrt(size))
         }
         this.size = size
         this.marbleColor = marbleColor
         this.squareColor = squareColor
+        this.x += this.x_speed*(72.5/marbleSpeed)
+        this.y += this.y_speed*(72.5/marbleSpeed)
     }
     draw(){
         let width = (5+Math.pow(this.size,0.4))
         let height = width
-        let sqs = squares.filter(square => square.x<=this.x+width/2+sqaureSize*2 && square.x>=this.x-width/2-sqaureSize*2 && square.y<=this.y+height/2+sqaureSize*2 && square.y>=this.y-height/2-sqaureSize*2)
+        let sqs = squares.filter(square => square.x<=this.x+width/2+sqaureSize*2+marbleSpeed && square.x>=this.x-width/2-sqaureSize*2-marbleSpeed && square.y<=this.y+height/2+sqaureSize*2+marbleSpeed && square.y>=this.y-height/2-sqaureSize*2-marbleSpeed)
         for (let i=0; i<sqs.length; i++){
             if (this.size>0){
                 let hit = sqs[i].collide(this.x-width/2,this.y-height/2,width,height,this.squareColor)
@@ -90,10 +97,10 @@ class Tower{
         this.startAngle = startAngle
         this.endAngle = endAngle
         setInterval(() => {
-            const chance = Math.random()
-            if (chance>0.5 && this.size<5000){this.size *= 2}
+            let chance = Math.random()
+            if (chance>Chance && this.size<5000){this.size *= 2}
             else{
-                this.marbles.push(new Marble(x,y,this.size,marbleColor,squareColor,this.angle))
+                this.marbles.push(new Marble(x,y,this.size,this.marbleColor,this.squareColor,this.angle))
                 this.size = 1
             }
         },1000)
@@ -141,6 +148,26 @@ function mainLoop(){
     yellowTower.draw()
     blueTower.draw()
     time += 1
+    greenTower.squareColor = squares[0].color
+    blueTower.squareColor = squares[2401].color
+    yellowTower.squareColor = squares[squares.length-1].color
+    redTower.squareColor = squares[98].color
+    if (greenTower.squareColor == '#ff0000'){greenTower.marbleColor = '#7d0000'}
+    if (greenTower.squareColor == '#0000ff'){greenTower.marbleColor = '#00007d'}
+    if (greenTower.squareColor == '#ffff00'){greenTower.marbleColor = '#7d7d00'}
+    if (greenTower.squareColor == '#00ff00'){greenTower.marbleColor = '#007d00'}
+    if (redTower.squareColor == '#00ff00'){redTower.marbleColor = '#007d00'}
+    if (redTower.squareColor == '#0000ff'){redTower.marbleColor = '#00007d'}
+    if (redTower.squareColor == '#ffff00'){redTower.marbleColor = '#7d7d00'}
+    if (redTower.squareColor == '#ff0000'){redTower.marbleColor = '#7d0000'}
+    if (blueTower.squareColor == '#ff0000'){blueTower.marbleColor = '#7d0000'}
+    if (blueTower.squareColor == '#00ff00'){blueTower.marbleColor = '#007d00'}
+    if (blueTower.squareColor == '#ffff00'){blueTower.marbleColor = '#7d7d00'}
+    if (blueTower.squareColor == '#0000ff'){blueTower.marbleColor = '#00007d'}
+    if (yellowTower.squareColor == '#ff0000'){yellowTower.marbleColor = '#7d0000'}
+    if (yellowTower.squareColor == '#0000ff'){yellowTower.marbleColor = '#00007d'}
+    if (yellowTower.squareColor == '#00ff00'){yellowTower.marbleColor = '#007d00'}
+    if (yellowTower.squareColor == '#ffff00'){yellowTower.marbleColor = '#7d7d00'}
     requestAnimationFrame(mainLoop)
 }
 mainLoop()
@@ -148,6 +175,8 @@ setInterval(() => {
     drawRect('#000000',0,0,200,1000)
     ctx.fillStyle = '#ffffff'
     ctx.font = '36px Arial'
-    ctx.fillText(time,75,75)
+    ctx.fillText(time+'    '+Chance.toFixed(2),5,75)
     time = 0
+    if (Chance>0.25){Chance -= 0.001}
 },1000)
+window.addEventListener('keydown', function(e){if (e.key == 'r'){location.reload()}})
